5bfe0b6a699839efb84d0c095b9f7e6e9175db93
philldeveloper <philldeveloper@gmail.com>
2020-07-10 17:37:57 -0300

    insercao de css, imagem e header em home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,31 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
-        </div>
+<header class="p-5 mx-auto">
+    <h1 class="display-3 text-center text-purple p-lg-5 p-md-0 p-sm-0">
+        Uma <span class="text-dark">agência</span> diferente.
+    </h1>
+    <p class="lead text-center text-dark py-3 mx-auto p-md-2 p-sm-2">
+        Somos mais que uma agência, somos uma equipe de soluções e resultados completos, 
+        onde diferentes talentos se unem conectando ideias, inspirações e conhecimentos para tocar 
+        as mentes e os corações de consumidores em todas as suas conexões, plataformas e estações.
+    </p>
+    <div class="text-center py-5">
+        <a href="#" class="btn btn-purple secular-font btn-large lead navlink">
+            VER MAIS
+        </a>
+        <a href="#" class="btn btn-link text-dark secular-font font-weight-bold btn-large lead navlink">
+            LOREM IPSUM
+        </a>
     </div>
-</div>
+</header>
+
+<section class="p-5 bg-light mx-auto">
+
+</section>
+
+<footer>
+
+</footer>
 @endsection
