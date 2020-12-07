182871fbbca97ee889086384371e7240bef135a4
Philip Ramon <philldeveloper@github.com>
2020-12-07 20:24:13 -0300

    0712

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,18 +15,15 @@
         as mentes e os corações de consumidores em todas as suas conexões, plataformas e estações.
     </p>
     <div class="text-center py-5">
-        <a href="#" class="btn btn-purple secular-font btn-large lead navlink">
-            VER MAIS
-        </a>
-        <a href="#" class="btn btn-link text-dark secular-font font-weight-bold btn-large lead navlink">
-            LOREM IPSUM
+        <a href="#" class="btn btn-outline-purple rounded-circle secular-font lead navlink" style="padding: 20px;">
+            <i class="fa fa-arrow-down"></i>
         </a>
     </div>
 </header>
 
 <section class="bg-light section-home" style="background:linear-gradient(0deg, rgba(128, 0, 128, 0.9), rgba(255, 0, 150, 0.3)), url('../img/creative.jpeg') no-repeat center center fixed; backgorund-size: cover">
     <h1 class="display-3 text-center text-white p-lg-5 p-md-0 p-sm-0">
-        O time mais <span class="text-warning">criativo</span>.
+        O time <i class="fa fa-plus p-2 text-warning"></i> criativo.
     </h1>
     <p class="lead text-center text-white montserrat-font py-3 mx-auto p-md-2 p-sm-2" style="font-size: 17pt !important;">
         A Atena Agência traz para você os melhores profissionais que usarão seus poderes de cavalheiros cibernéticos
@@ -34,84 +31,72 @@
         gráfico, campanhas publicitárias, materiais gráfico e planejamento estratégico.
     </p>
     <div class="text-center py-3">
-        <a href="#" class="btn btn-outline-warning rounded-circle secular-font lead navlink" style="padding: 20px;">
-            <i class="fa fa-arrow-down"></i>
+        <a href="#" class="btn btn-outline-light rounded-circle secular-font lead navlink" style="padding: 20px;">
+            <i class="fa fa-star"></i>
         </a>
     </div>
 </section>
 
 
 <section class="bg-white section-ourjob p-5 text-purple">
-    <h1 class="display-3 text-center p-lg-5 p-md-0 p-sm-0">
-        O que nós <span class="text-info">fazemos</span>.
-    </h1>
-    <p class="lead text-center montserrat-font py-3 mx-auto p-md-2 p-sm-2 mb-5" style="font-size: 17pt !important;">
-        A Atena Agência traz para você os melhores profissionais que usarão seus poderes de cavalheiros cibernéticos
-        como: desenvolvimento web, palestras e treinamentos, marketing e mídias digitais, assessoria de imprensa, design
-        gráfico, campanhas publicitárias, materiais gráfico e planejamento estratégico.
-    </p>
+    <div class="container">
+        <h1 class="display-4 text-center py-4">
+            O que nós <span class="">fazemos</span>
+        </h1>
+    </div>
+    <div class="container pt-4 pb-0 text-light">
+        <!--first line of posts-->
+        <div class="row py-4">
+            <div class="col-md-3">
+                <div class="card h-100 border-0 p-5" style="display: flex; align-items: center; justify-content: center; background: url('https://images.pexels.com/photos/3981477/pexels-photo-3981477.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260') no-repeat center center fixed; background-size: cover;">
+                    <h2 class="montserrat-font">Marketing Digital</h2>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="card h-100 border-0 p-5" style="display: flex; align-items: center; justify-content: center; background: url('https://images.pexels.com/photos/3981477/pexels-photo-3981477.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260') no-repeat center center fixed; background-size: cover;">
+                    <h2 class="montserrat-font text-center">Publicidade e Propaganda</h2>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="card h-100 border-0 p-5" style="display: flex; align-items: center; justify-content: center; background: url('https://images.pexels.com/photos/3981477/pexels-photo-3981477.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260') no-repeat center center fixed; background-size: cover;">
+                    <h2 class="montserrat-font">Identidade Visual</h2>
+                </div>
+            </div>
+        </div><!--end first line of posts-->
+
+        <!--second line of posts-->
+        <div class="row pb-3">
+            <div class="col-md-4">
+                <div class="card h-100 border-0 p-5" style="display: flex; align-items: center; justify-content: center; background: url('https://images.pexels.com/photos/3981477/pexels-photo-3981477.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260') no-repeat center center fixed; background-size: cover;">
+                    <h2 class="montserrat-font">Lorem Ipsum</h2>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="card h-100 border-0 p-5" style="display: flex; align-items: center; justify-content: center; background: url('https://images.pexels.com/photos/3981477/pexels-photo-3981477.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260') no-repeat center center fixed; background-size: cover;">
+                    <h2 class="montserrat-font">Desenvolvimento Web</h2>
+                </div>
+            </div>
+            <div class="col-md-2">
+                <div class="card h-100 border-0 p-5" style="display: flex; align-items: center; justify-content: center; background: url('https://images.pexels.com/photos/3981477/pexels-photo-3981477.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260') no-repeat center center fixed; background-size: cover;">
+                    <h2 class="montserrat-font">Lorem Ipsum</h2>
+                </div>
+            </div>
+        </div><!--end second line of posts-->
+    </div>
+
+    <div class="container py-5">
+        <p class="lead text-center montserrat-font mx-auto p-md-2 p-sm-2" style="font-size: 17pt !important;">
+            A Atena Agência traz para você os melhores profissionais que usarão seus poderes de cavalheiros cibernéticos
+            como: desenvolvimento web, palestras e treinamentos, marketing e mídias digitais, assessoria de imprensa, design
+            gráfico, campanhas publicitárias, materiais gráfico e planejamento estratégico.
+        </p>
+    </div>
     <div class="text-center py-3">
         <a href="#" class="btn btn-purple rounded-circle secular-font lead navlink" style="padding: 20px;">
             <i class="fa fa-arrow-down"></i>
         </a>
     </div>
 </section>
-
-<section class="bg-purple section-ourjob p-5 text-light">
-    <div class="col-10 mx-auto mt-5 mb-5">
-    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ea odio magni, fugiat eos eveniet sequi quaerat. Animi magni suscipit repellendus veritatis, enim minus sit maxime corrupti debitis sunt nisi consequatur?</p>
-    </div>
-</section>
-
-{{-- <section class="bg-purple section-ourjob p-5 text-light">
-    <div class="col-10 mx-auto mt-5 mb-5">
-        <div class="row">
-            <div class="col-md-4 col-sm-12 bg-transparent">
-                <h3 class="text-light py-3">Marketing Digital</h3>
-                <div class="list-group bg-transparent border-light border" style="border-radius: 0px 0px 5px 5px; border-top: 10px solid white !Important">
-                    <a href="#" class="list-group-item list-group-item-action bg-transparent text-light border-0 montserrat-font lead">- Gerenciamento de Redes Sociais</a>
-                    <a href="#" class="list-group-item list-group-item-action bg-transparent text-light border-0 montserrat-font lead">- Assessoria Digital</a>
-                    <a href="#" class="list-group-item list-group-item-action bg-transparent text-light border-0 montserrat-font lead">- SEO</a>
-                </div>
-                <a href="#" class="btn btn-light mt-3 font-weight-bold">ver mais</a>
-            </div>
-            <div class="col-md-4 col-sm-12">
-                <h3 class="text-light py-3">Publicidade e Propaganda</h3>
-                <div class="list-group border-0">
-                    <a href="#" class="list-group-item list-group-item-action text-dark lead border-0 font-weight-bold">- Campanha Publicitária</a>
-                    <a href="#" class="list-group-item list-group-item-action text-dark lead border-0 font-weight-bold">- Assessoria de Comunicação</a>
-                    <a href="#" class="list-group-item list-group-item-action text-dark lead border-0 font-weight-bold">- Planejamento Estratégico e Consultoria</a>
-                </div>
-                <a href="#" class="btn btn-light mt-3 font-weight-bold">ver mais</a>
-            </div>
-            <div class="col-md-4 col-sm-12">
-                <h3 class="text-light py-3">Identidade Visual</h3>
-                <div class="list-group">
-                <!-- <div class="list-group" style="border-top: 8px solid transparent; border-image: url(../img/creative.jpeg) 30 stretch;"> -->
-                    
-                    <a href="#" class="list-group-item list-group-item-action text-dark lead font-weight-bold">Criação de Logotipos</a>
-                    <a href="#" class="list-group-item list-group-item-action text-dark lead font-weight-bold">Criação de Cartões de Visita</a>
-                    <a href="#" class="list-group-item list-group-item-action text-dark lead font-weight-bold">Manuais de Uso e Manual de Marca</a>
-                </div>
-                <a href="#" class="btn btn-light mt-3 font-weight-bold">ver mais</a>
-            </div>
-            <div class="col-md-4 col-sm-12">
-                <h3 class="text-light py-3">Desenvolvimento Web</h3>
-                <div class="list-group">
-                    <a href="#" class="list-group-item list-group-item-action text-dark lead font-weight-bold">Criação de Sistemas</a>
-                    <a href="#" class="list-group-item list-group-item-action text-dark lead font-weight-bold">Criação de Websites</a>
-                    <a href="#" class="list-group-item list-group-item-action text-dark lead font-weight-bold">Criação de Aplicativos</a>
-                </div>
-                <a href="#" class="btn btn-light mt-3 font-weight-bold">ver mais</a>
-            </div>
-        </div>
-    </div>
-    <div class="text-center py-3">
-        <a href="#" class="btn btn-outline-light rounded-circle secular-font lead navlink" style="padding: 20px;">
-            <i class="fa fa-arrow-down font-weight-bold"></i>
-        </a>
-    </div>
-</section> --}}
 
 
 <section class="bg-light section-comments">
@@ -156,11 +141,10 @@
     </div>
 </section>
 
-<section class="p-5">
+<!-- <section class="p-5">
     <div class="mx-auto">
         <h1 class="lead secular-font py-3 mx-auto p-md-2 p-sm-2 text-purple display-4">Alguns de nossos clientes</h1>
     </div>
-    
     <div class="carousel" data-flickity='{ "wrapAround": true, "autoPlay": true }'>
         <div class="carousel-cell d-flex align-items-center justify-content-center">
             <img src="{{asset('img/logo_atena.png')}}" alt="">
@@ -181,7 +165,7 @@
             <img src="{{asset('img/logo_atena.png')}}" alt="">
         </div>
     </div>
-</section>
+</section> -->
 
 
 <link rel="stylesheet" href="https://unpkg.com/flickity@2/dist/flickity.min.css">
@@ -233,7 +217,4 @@
         display: none
     }
 </style>
-
-
-
 @endsection
